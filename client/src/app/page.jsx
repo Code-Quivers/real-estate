55ba969ae8c1d03151c483f4a61b379881ce20d4
Md Rafiqul Islame <rafiqulislam11002@gmail.comm>
2023-12-11 14:30:04 +0600

    main page height reducred

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -29,18 +29,15 @@ const HomePage = () => {
         <h1 className="text-5xl md:text-5xl lg:text-6xl font-bold max-lg:text-center ">
           Which best describes you?
         </h1>
-        <Button appearance="primary" color="cyan">
-          Primary
-        </Button>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-5 mt-10 lg:mt-20 justify-center max-lg:mx-5 max-w-7xl mx-auto  ">
+      <div className="grid grid-cols-1 md:grid-cols-3  xl:grid-cols-3 gap-5 mt-10 lg:mt-20 justify-center max-lg:mx-5 max-w-6xl mx-auto  ">
         {allData.map((data) => (
           <div
             key={Math.random()}
             className="col-span-1 flex justify-center rounded-[40px] lg:rounded-[60px] items-center overflow-hidden"
           >
             <Link
-              className="w-full grid grid-cols-1 justify-center  p-5 py-7 lg:py-10 rounded-[40px] lg:rounded-[60px]  bg-[#29429f]  hover:bg-[#1d3796] transition-all duration-400 ease-in-out  items-center  "
+              className="w-full grid grid-cols-1 justify-center  p-5 py-5 lg:py-6 rounded-[40px] lg:rounded-[60px]  bg-[#29429f]  hover:bg-[#1d3796] transition-all duration-400 ease-in-out  items-center  "
               key={Math.random()}
               href={data.href}
             >
