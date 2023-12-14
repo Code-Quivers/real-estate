736d054f66a51143c1a32f512d6eb204ea2c6284
shourovr82 <shafinur512@gmail.com>
2023-12-14 10:04:53 +0600

    [DONE] service provider added

--- client/src/app/property-owner/(property-owner-layout)/saved-service-providers/page.jsx
+++ client/src/app/property-owner/(property-owner-layout)/saved-service-providers/page.jsx
@@ -1,14 +1,23 @@
 "use client";
 import { FaSearch } from "react-icons/fa";
-import { Popover, Whisper } from "rsuite";
+import { Popover, SelectPicker, Whisper } from "rsuite";
 import { AutoComplete, InputGroup } from "rsuite";
 import profileLogo from "@/assets/propertyOwner/profilePic.png";
 import Image from "next/image";
 import { useState } from "react";
-
-import AvailableTenantsModal from "@/components/property-owner/available-tenants/AvailableTenantsModal";
+import MaintenanceServiceProviderModal from "@/components/property-owner/maintenance-request/MaintenanceServiceProviderModal";
 
 const PropertyOwnerServiceProviders = () => {
+  const data = [
+    "Eugenia",
+    "Bryan",
+    "Linda",
+    "Nancy",
+    "Lloyd",
+    "Alice",
+    "Julia",
+    "Albert",
+  ].map((item) => ({ label: item, value: item }));
   const datas = [
     "Eugenia",
     "Bryan",
@@ -71,53 +80,13 @@ const PropertyOwnerServiceProviders = () => {
   const [selectedService, setSelectedService] = useState(null);
 
   return (
-    <section className="max-w-[1050px]  max-lg:px-3   pb-20 mx-auto mb-5 mt-6 lg:mt-8 2xl:mx-auto lg:px-5    2xl:px-0 ">
+    <section className="max-w-[1050px]  max-lg:px-3   pb-20 mx-auto mb-5 mt-6 lg:mt-5 2xl:mx-auto lg:px-5    2xl:px-0 ">
       <div className="flex justify-center">
-        <h2 className="text-3xl ">Available Tenants</h2>
-      </div>
-      {/* search with price section start */}
-      <div className="grid grid-cols-7 gap-0.5 max-lg:gap-2 lg:flex w-full   mt-5 lg:mt-5   ">
-        {/* tenant name */}
-        <div className="max-lg:col-span-3 col-span-3 w-full">
-          <InputGroup size="lg" inside style={{ borderRadius: "0 !important" }}>
-            <AutoComplete placeholder="Tenant Name" size="lg" data={datas} />
-            <InputGroup.Addon style={{ backgroundColor: "#fff" }}>
-              <FaSearch size={20} />
-            </InputGroup.Addon>
-          </InputGroup>
-        </div>
-        {/* address */}
-        <div className="max-lg:col-span-2 col-span-2 w-full">
-          <InputGroup
-            size="lg"
-            inside
-            className="lg:!w-full"
-            style={{ borderRadius: "0 !important" }}
-          >
-            <AutoComplete placeholder="Address" size="lg" data={datas} />
-            <InputGroup.Addon style={{ backgroundColor: "#fff" }}>
-              <FaSearch size={20} />
-            </InputGroup.Addon>
-          </InputGroup>
-        </div>
-        {/* rent */}
-        <div className="max-lg:col-span-2 col-span-2  w-full ">
-          <InputGroup
-            size="lg"
-            inside
-            className="lg:!w-full "
-            style={{ borderRadius: "0 !important" }}
-          >
-            <AutoComplete placeholder="Rent" size="lg" data={datas} />
-            <InputGroup.Addon style={{ backgroundColor: "#fff" }}>
-              <FaSearch size={20} />
-            </InputGroup.Addon>
-          </InputGroup>
-        </div>
+        <h2 className="text-3xl ">SavedService Providers</h2>
       </div>
 
       {/* all cards */}
-      <div className="mt-10 grid grid-cols-1 lg:grid-cols-2 gap-5">
+      <div className="mt-5 grid grid-cols-1 lg:grid-cols-2 gap-5">
         {allRequest?.map((singleReq) => (
           <Whisper
             placement="bottom"
@@ -128,12 +97,13 @@ const PropertyOwnerServiceProviders = () => {
                 <div className="flex gap-2 border rounded-2xl overflow-hidden shadow items-stretch">
                   <div>
                     <Image
-                      className="w-[80px] bg-red-800 h-full object-cover     "
+                      className="w-[100px] bg-red-800 h-full object-cover     "
                       src={singleReq?.image}
                       alt="photo"
                     />
                   </div>
                   <div className="py-2">
+                    <h3 className="text-base ">${singleReq?.servicePrice}</h3>
                     <h3 className="text-base ">3 Bed 3 Bath</h3>
                     <h3 className="text-base w-[80%]">
                       3 Belair Dr, Binghamton, NY 13901
@@ -146,12 +116,11 @@ const PropertyOwnerServiceProviders = () => {
             key={Math.random()}
           >
             <div
-              key={Math.random()}
               onClick={() => {
                 setSelectedService(singleReq);
                 setServiceModalActive(true);
               }}
-              className=" col-span-1  border flex justify-between items-center pl-3 pr-5 shadow-lg  rounded-3xl border-[#acacac]  gap-2"
+              className=" col-span-1  shadow-lg border flex justify-between items-center px-5 border-[#acacac]  gap-2"
             >
               <div>
                 <Image
@@ -160,14 +129,20 @@ const PropertyOwnerServiceProviders = () => {
                   alt="photo"
                 />
               </div>
-              <div className="p-3 flex justify-between w-full ">
+              <div className="p-5 flex justify-between w-full ">
                 <div className="space-y-0.5">
-                  <h3 className="text-sm font-medium">Tenant Name</h3>
-                  <h3 className="text-sm font-medium">Place to rent</h3>
-                  <h3 className="text-sm font-medium">Rent willing to pay</h3>
+                  <h3 className="text-base font-medium">
+                    {singleReq?.serviceProviderName}
+                  </h3>
+                  <h3 className="text-base font-medium">
+                    {singleReq?.serviceType}
+                  </h3>
+                  <h3 className="text-base font-medium">
+                    Service Price : ${singleReq?.servicePrice}
+                  </h3>
                 </div>
               </div>
-              <div className=" outline outline-[6px] outline-[#58ba66] border  ring-[#33333360] ring border-[#33333360]  rounded-full   flex justify-center items-center  w-[70px] h-[50px]">
+              <div className=" outline outline-[6px] outline-[#58ba66] border  ring-[#33333360] ring border-[#33333360]  rounded-full   flex justify-center items-center  w-[75px] h-[50px]">
                 <div className=" flex w-full flex-col justify-center items-center">
                   <span>9</span>
                   <span className="w-[70%] border-t border-[#b6b6b6]" />
@@ -179,7 +154,7 @@ const PropertyOwnerServiceProviders = () => {
         ))}
 
         <>
-          <AvailableTenantsModal
+          <MaintenanceServiceProviderModal
             isModalOpened={serviceModalActive}
             setModalOpened={setServiceModalActive}
             modalData={selectedService}
