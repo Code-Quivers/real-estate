baa27da319e02b284c74cb07a7e9816967144bc9
shourovr82 <shafinur512@gmail.com>
2023-12-23 22:57:18 +0600

    [DONE] bug fixed

--- server/src/app/modules/auth/auth.routes.ts
+++ server/src/app/modules/auth/auth.routes.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { AuthController } from './auth.controller';
+import auth from '../../middlewares/auth';
+import { UserRoles } from '@prisma/client';
 // import { UserRoles } from '@prisma/client';
 // import auth from '../../middlewares/auth';
 
@@ -30,6 +32,15 @@ router.post(
 
 router.post('/login', AuthController.userLogin);
 
-router.post('/refresh-token', AuthController.refreshToken);
+router.post(
+  '/refresh-token',
+  auth(
+    UserRoles.SUPERADMIN,
+    UserRoles.SERVICE_PROVIDER,
+    UserRoles.PROPERTY_OWNER,
+    UserRoles.TENANT
+  ),
+  AuthController.refreshToken
+);
 
 export const AuthRoutes = router;
