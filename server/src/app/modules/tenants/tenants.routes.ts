57405ab745867a9c10c026b48cc5c24234de7663
shourovr82 <shafinur512@gmail.com>
2023-12-29 22:35:32 +0600

    [DONE] tenant my profile data get completed

--- server/src/app/modules/tenants/tenants.routes.ts
+++ server/src/app/modules/tenants/tenants.routes.ts
@@ -9,11 +9,9 @@ const router = express.Router();
 // ! get all tenants
 router.get("/get-all-tenants", auth(UserRoles.PROPERTY_OWNER, UserRoles.SERVICE_PROVIDER, UserRoles.SUPERADMIN), TenantsControllers.getAllTenants);
 // ! get single tenant
-router.get(
-  "/get-single-tenant/:tenantId",
-  auth(UserRoles.PROPERTY_OWNER, UserRoles.PROPERTY_OWNER, UserRoles.SERVICE_PROVIDER, UserRoles.SUPERADMIN),
-  TenantsControllers.getSingleTenant,
-);
+router.get("/get-single-tenant/:tenantId", auth(UserRoles.PROPERTY_OWNER, UserRoles.SERVICE_PROVIDER, UserRoles.SUPERADMIN), TenantsControllers.getSingleTenant);
+// ! get tenant my profile
+router.get("/get-my-profile", auth(UserRoles.TENANT), TenantsControllers.getTenantMyProfile);
 // ! update tenant profile
 router.patch(
   "/update-profile/:tenantId",
