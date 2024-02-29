152a186db900dd79e5c5732be8a9c5bdeaf18aa9
shourovr82 <shafinur512@gmail.com>
2024-02-29 12:42:21 +0600

    [bug ]

--- client/src/components/property-owner/unit-information/UpdateImageUpload.jsx
+++ client/src/components/property-owner/unit-information/UpdateImageUpload.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable prefer-const */
 "use client"; // UpdateImageUpload.jsx
 import { Uploader, Button } from "rsuite";
 import { Controller } from "react-hook-form";
@@ -6,7 +5,7 @@ import { fileUrlKey } from "@/configs/envConfig";
 import { useState } from "react";
 
 const UpdateImageUpload = ({ control, setValue, images }) => {
-  let [defaultFileList, setDefaultFileList] = useState(
+  const [defaultFileList, setDefaultFileList] = useState(
     images?.map((image, index) => ({
       fileKey: `default-${index + 1}`,
       name: `Image ${index + 1}`,
@@ -15,7 +14,15 @@ const UpdateImageUpload = ({ control, setValue, images }) => {
   );
 
   const handleRemove = (file) => {
-    console.log("Removing file:", file);
+    // Remove the file from the defaultFileList
+    // const updatedDefaultFileList = defaultFileList.filter((item) => item.fileKey !== file.fileKey).map((item) => ({ ...item }));
+
+    // // Update the defaultFileList
+    // setDefaultFileList(updatedDefaultFileList);
+
+    // // Remove the file from the "files" field in the React Hook Form
+    // setValue("files", updatedDefaultFileList);
+    console.log(file);
 
     if (file.fileKey.startsWith("default-")) {
       // Remove the file from the defaultFileList
@@ -26,10 +33,15 @@ const UpdateImageUpload = ({ control, setValue, images }) => {
 
       // Remove the file from the "files" field in the React Hook Form
       setValue("files", updatedDefaultFileList);
-    } else if (!file.fileKey.startsWith("default-")) {
+    } else {
       // Handle the removal of selected files
-      const updatedSelectedFiles = defaultFileList.filter((item) => item.fileKey !== file.fileKey);
-
+      const updatedSelectedFiles = defaultFileList.filter((item) => {
+        console.log("item", item.fileKey);
+        console.log("file", file.fileKey);
+        return item.fileKey !== file.fileKey;
+      });
+      console.log(updatedSelectedFiles);
+      console.log(updatedSelectedFiles, "selected");
       // Update the "files" field in the React Hook Form
       setValue("files", updatedSelectedFiles);
 
@@ -37,29 +49,26 @@ const UpdateImageUpload = ({ control, setValue, images }) => {
       setDefaultFileList(updatedSelectedFiles);
     }
   };
-
+  console.log(defaultFileList);
   const handleChangeImages = (files) => {
     if (files.length > 0) {
       const fileSizeLimit = 512 * 2 * 1024; // 1 MB
 
-      // Check if any file is not from the existing selected file list
-      const isNotFromSelected = files.some(
-        (file) => !defaultFileList.some((defaultFile) => defaultFile.fileKey.startsWith("selected") && defaultFile.url === file.url),
-      );
-
-      if (isNotFromSelected) {
-        // Filter and map validBlobFiles
+      // Check if any file is from the new selected files
+      const isNewSelected = files.some((file) => defaultFileList.every((defaultFile) => defaultFile.url !== file.url));
+      if (isNewSelected) {
+        console.log("is new selected");
         const validBlobFiles = files
           .filter((filetype) => filetype?.blobFile?.size && filetype?.blobFile?.size <= fileSizeLimit)
           .map((filetype, index) => ({
             blobFile: filetype.blobFile,
-            fileKey: `selected-${new Date().getTime()}-${index + 1}`, // Use a unique key
+            fileKey: `selected-${index + 1}`,
             name: filetype.blobFile.name,
-            status: "inited",
+            status: "inited", // You might want to set an appropriate status
           }));
 
         // Update the defaultFileList with new and valid files
-        const updatedDefaultFileList = [...defaultFileList, ...validBlobFiles];
+        const updatedDefaultFileList = [...defaultFileList.filter((file) => !file.fileKey.startsWith("selected")), ...validBlobFiles];
 
         // Update the "files" field in the React Hook Form
         setValue("files", updatedDefaultFileList);
