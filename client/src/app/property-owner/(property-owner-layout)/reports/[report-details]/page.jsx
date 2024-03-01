1f22850656344dc61fd4272c140089bd31d19423
mostafizcw <mostafizur.rahaman.cw@gmail.com>
2024-03-01 14:02:58 +0600

    [DONE] report page done

---
+++ client/src/app/property-owner/(property-owner-layout)/reports/[report-details]/page.jsx
@@ -0,0 +1,11 @@
+import PropertyStatement from "@/components/reports/PropertyStatement";
+
+const ReportDetails = () => {
+  return (
+    <>
+      <PropertyStatement />
+    </>
+  );
+};
+
+export default ReportDetails;
