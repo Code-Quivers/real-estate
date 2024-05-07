c7fdd7072084fae51eadb6ad124de95d936fe07b
shourovr82 <shafinur512@gmail.com>
2024-05-08 00:29:21 +0600

    [DONE] tenant documents

--- server/prisma/migrations/20240506152735_db/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `drivingLicenseNumber` on the `tenants` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "tenants" DROP COLUMN "drivingLicenseNumber",
-ADD COLUMN     "placeToRent" TEXT;
