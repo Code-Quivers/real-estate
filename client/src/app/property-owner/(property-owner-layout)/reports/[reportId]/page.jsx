cfa068599d78fa47e46ecb4d2313e51f0ff42410
mostafizcw <mostafizur.rahaman.cw@gmail.com>
2024-05-12 18:42:32 +0600

    [DONE] reports design updated

--- client/src/app/property-owner/(property-owner-layout)/reports/[reportId]/page.jsx
+++ client/src/app/property-owner/(property-owner-layout)/reports/[reportId]/page.jsx
@@ -17,7 +17,7 @@ const TenantInformationReportPage = ({ params }) => {
   );
   return (
     <section className="max-w-[1050px] min-h-screen mb-5  xl:mx-auto md:px-3 lg:px-5 px-2 2xl:px-0">
-      <div className=" flex justify-center my-10">
+      <div className=" flex justify-center my-2">
         <h2>Reports</h2>
       </div>
       {/* if report type is tenant_info */}
--- client/src/components/reports/MonthlyReportDetails.jsx
+++ client/src/components/reports/MonthlyReportDetails.jsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { FaUser } from "react-icons/fa";
 import { IoChevronBack } from "react-icons/io5";
-import { Button } from "rsuite";
+import { Avatar, Button } from "rsuite";
 const MonthlyReportDetails = ({ reportData }) => {
   const router = useRouter();
   return (
-    <section className="border bg-white md:p-3 lg:p-8">
+    <section>
       <div className="grid grid-cols-7 max-md:p-3 items-center">
-        <div className="col-span-2">
+        <div className="col-span-1">
           <button onClick={() => router.back()}>
             <IoChevronBack size={24} />
           </button>
@@ -19,18 +19,18 @@ const MonthlyReportDetails = ({ reportData }) => {
         </div>
       </div>
       {/* details */}
-      <div className="border m-3 md:m-6 shadow rounded-md  md:h-60 p-2 bg-white grid md:grid-cols-7 md:gap-5 xl:gap-10">
-        <div className="md:col-span-3">
+      <div className="border m-3 md:m-6 shadow rounded-md  md:h-60 p-2 bg-white grid md:grid-cols-12 md:gap-5 xl:gap-10">
+        <div className="md:col-span-5 h-[220px]">
           <Image
-            className="h-full  object-cover rounded-md"
+            className="h-full w-full object-cover rounded-md"
             src={`${fileUrlKey()}/${reportData?.information[0]?.image}`}
-            width={400}
-            height={400}
+            width={800}
+            height={800}
             alt="apartment"
           />
         </div>
 
-        <div className="md:col-span-4 py-6">
+        <div className="md:col-span-7 py-6">
           {reportData?.information?.map((information) => (
             <div key={Math.random()} className="space-y-2">
               <p>$ {information?.monthlyRent}</p>
@@ -38,15 +38,16 @@ const MonthlyReportDetails = ({ reportData }) => {
                 {information?.numOfBed} Beds {information?.numOfBath} Baths
               </p>
               <p>{information?.address}</p>
-              <div className="border    rounded-md p-2 flex gap-3 items-center">
+              <div className="border bg-gray-50    rounded-md p-2 flex gap-3 items-center">
                 <div className="w-12 h-12 rounded-full  flex items-center justify-center">
-                  {information?.tenantPhoto ? (
+                  <Avatar circle size="md" src={information?.tenantPhoto && `${fileUrlKey()}/${information?.tenantPhoto}`} />
+                  {/* {information?.tenantPhoto ? (
                     <Image src={`${fileUrlKey()}/${information?.tenantPhoto}`} width={100} height={100} className="rounded-full" />
                   ) : (
                     <span>
                       <FaUser size={20} className="text-gray-500" />
                     </span>
-                  )}
+                  )} */}
                 </div>
                 <p>{information?.tenantName}</p>
               </div>
@@ -54,25 +55,25 @@ const MonthlyReportDetails = ({ reportData }) => {
           ))}
         </div>
       </div>
-      <div className="grid grid-cols-4 max-md:px-3 gap-x-4 lg:gap-x-10 gap-y-5 max-md:mt-5">
-        <div className="col-span-2 flex items-center justify-between p-3 border rounded-lg">
+      <div className="grid sm:grid-cols-4 px-5 gap-x-4 lg:gap-x-10 gap-y-5 max-md:mt-5">
+        <div className="col-span-2 bg-white flex items-center justify-between p-3 border rounded-lg">
           <h2>Monthly Rent</h2>
           <p className="text-lg font-semibold">${reportData?.rentAmount}</p>
         </div>
-        <div className="col-span-2 flex items-center justify-between p-3 border rounded-lg">
+        <div className="col-span-2 bg-white flex items-center justify-between p-3 border rounded-lg">
           <h2>Monthly Expenses</h2>
           <p className="text-lg font-semibold">${reportData?.expenses}</p>
         </div>
-        <div className="col-span-2 flex items-center justify-between p-3 border rounded-lg">
+        <div className="col-span-2 bg-white flex items-center justify-between p-3 border rounded-lg">
           <h2>Rent Collected</h2>
           <p className="text-lg font-semibold">${reportData?.collectedRent}</p>
         </div>
-        <div className="col-span-2 flex items-center justify-between p-3 border rounded-lg">
+        <div className="col-span-2 bg-white flex items-center justify-between p-3 border rounded-lg">
           <h2>Gross Profit</h2>
           <p className="text-lg font-semibold">${reportData?.grossProfit}</p>
         </div>
       </div>
-      <div className="mt-5 flex justify-center border rounded-xl p-5">
+      <div className="mt-5 flex justify-center p-5 mx-5 text-green-500 font-bold text-lg">
         <h2>{`Your're`} Profitable</h2>
       </div>
 
