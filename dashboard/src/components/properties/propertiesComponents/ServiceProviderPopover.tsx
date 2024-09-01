7acc27d18cf4b4cf23c21c700ca46fd2cca46662
mostafizcw <mostafizur.rahaman.cw@gmail.com>
2024-09-01 23:36:14 +0600

    [DONE] modal of properties edit bug fixed

--- dashboard/src/components/properties/propertiesComponents/ServiceProviderPopover.tsx
+++ dashboard/src/components/properties/propertiesComponents/ServiceProviderPopover.tsx
@@ -19,31 +19,35 @@ const ServiceProviderPopover = ({ row }: any) => {
   ));
   return (
     <>
-      <Popover>
-        <Popover.Target>
-          <Button variant="transparent" size="compact-xs">
-            Click
-          </Button>
-        </Popover.Target>
-        <Popover.Dropdown>
-          {row.original.serviceProviders.map(
-            (serviceProvider: any, index: number) => (
-              <div key={index} className="space-y-2">
-                <div className="flex text-xs border rounded-md p-2 gap-7 shadow">
-                  <div className="">
-                    <p>{`${serviceProvider?.firstName} ${serviceProvider?.lastName}`}</p>
-                    <p>{serviceProvider?.user?.email}</p>
-                  </div>
-                  <div>
-                    <p>{`${serviceProvider?.companyName}`}</p>
-                    <p>{`${serviceProvider?.companyPhoneNumber}`}</p>
+      {row?.original?.serviceProviders?.length > 0 ? (
+        <Popover>
+          <Popover.Target>
+            <Button variant="transparent" size="compact-xs">
+              View
+            </Button>
+          </Popover.Target>
+          <Popover.Dropdown>
+            {row.original.serviceProviders.map(
+              (serviceProvider: any, index: number) => (
+                <div key={index} className="space-y-2">
+                  <div className="flex text-xs border rounded-md p-2 gap-7 shadow">
+                    <div className="">
+                      <p>{`${serviceProvider?.firstName} ${serviceProvider?.lastName}`}</p>
+                      <p>{serviceProvider?.user?.email}</p>
+                    </div>
+                    <div>
+                      <p>{`${serviceProvider?.companyName}`}</p>
+                      <p>{`${serviceProvider?.companyPhoneNumber}`}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )
-          )}
-        </Popover.Dropdown>
-      </Popover>
+              )
+            )}
+          </Popover.Dropdown>
+        </Popover>
+      ) : (
+        <p>N/A</p>
+      )}
     </>
   );
 };
