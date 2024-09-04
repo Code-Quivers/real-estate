28b9c0c7773d864a778c8aedd5f1ea3e06fbb403
MashodRana <mashod0rana@gmail.com>
2024-09-04 21:46:13 +0600

    updated configuration and docker file

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,6 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/dashboard",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "7000",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "77.237.234.238",
+        port: "7000",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "managerentalunit.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "managerentalunit.com/backend",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "managerentalunit.com/backend/api/v1",
+        pathname: "/**",
+      },
+    ],
+    domains: [
+      "localhost",
+      "77.237.234.238",
+      "77.237.234.238:7000",
+      "managerentalunit.com/backend/",
+      "managerentalunit.com/backend/api/v1",
+      "managerentalunit.com/backend/api/v1/",
+    ],
+  },
 };
 
 export default nextConfig;
